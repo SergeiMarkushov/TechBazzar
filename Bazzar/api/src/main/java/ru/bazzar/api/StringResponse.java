1d4fa1ebc2f0b539d9bdbc380f04fb2858668185
SeregaMarkush <s.markushov@icloud.com>
2023-05-23 20:14:32 +0300

    1)В модуле auth-service переписал properties на yml. 2)Все yml файлы убрал из игнора. 3)Во всех yml файлах подставить переменную среды вместо паролей, логинов, секретного ключа 4)Изменил URL подключения к БД, для создания отдельных БД для модулей 5)Подключил Redis к модулю корзины 6)Добавил строчку в миграцию для создания БД в модуль auth-service 7)Исправил синтаксическую ошибку на фронте

---
+++ Bazzar/api/src/main/java/ru/bazzar/api/StringResponse.java
@@ -0,0 +1,20 @@
+package ru.bazzar.api;
+
+public class StringResponse {
+    private String value;
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+
+    public StringResponse() {
+    }
+
+    public StringResponse(String value) {
+        this.value = value;
+    }
+}
--- Bazzar/cart-service/src/main/java/ru/bazzar/cart/controllers/CartController.java
+++ Bazzar/cart-service/src/main/java/ru/bazzar/cart/controllers/CartController.java
@@ -4,8 +4,11 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.web.bind.annotation.*;
 import ru.bazzar.api.CartDto;
+import ru.bazzar.api.StringResponse;
 import ru.bazzar.cart.converters.CartConverter;
 import ru.bazzar.cart.services.CartService;
+
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/api/v1/cart")
@@ -15,29 +18,48 @@ public class CartController {
     private final CartService cartService;
     private final CartConverter cartConverter;
 
-    @GetMapping("/add/{id}")
-    public void addToCart(@RequestHeader(name = "username") String username, @PathVariable Long id) {
-        cartService.add(username, id);
+
+    @GetMapping("/generate_uuid")
+    public StringResponse generateUuid() {
+        return new StringResponse(UUID.randomUUID().toString());
     }
 
-    @GetMapping("/clear")
-    public void clearCart(@RequestHeader(name = "username") String username) {
-        cartService.clear(username);
+    @GetMapping("/{uuid}/add/{id}")
+    public void addToCart(@RequestHeader(name = "username", required = false) String username, @PathVariable String uuid, @PathVariable Long id) {
+        String targetUuid = getCartUuid(username, uuid);
+        cartService.add(targetUuid, id);
     }
 
-    @GetMapping("/remove/{id}")
-    public void removeFromCart(@RequestHeader(name = "username") String username, @PathVariable Long id) {
-        cartService.remove(username, id);
+    @GetMapping("/{uuid}/clear")
+    public void clearCart(@RequestHeader(name = "username", required = false) String username, @PathVariable String uuid) {
+        String targetUuid = getCartUuid(username, uuid);
+        cartService.clear(targetUuid);
     }
 
-    @GetMapping
-    public CartDto getCurrentCart(@RequestHeader(name = "username") String username) {
-        return cartConverter.entityToDto(cartService.getCurrentCart(username));
+    @GetMapping("/{uuid}/remove/{id}")
+    public void removeFromCart(@RequestHeader(name = "username", required = false) String username, @PathVariable String uuid, @PathVariable Long id) {
+        String targetUuid = getCartUuid(username, uuid);
+        cartService.remove(targetUuid, id);
     }
 
-    @GetMapping("/change_quantity")
-    public void changeQuantity(@RequestHeader(name = "username") String username, @RequestParam Long productId, @RequestParam Integer delta) {
-        cartService.changeQuantity(username, productId, delta);
+    @GetMapping("/{uuid}")
+    public CartDto getCurrentCart(@RequestHeader(name = "username", required = false) String username, @PathVariable String uuid) {
+        String targetUuid = getCartUuid(username, uuid);
+        return cartConverter.entityToDto(cartService.getCurrentCart(targetUuid));
+    }
+
+    @GetMapping("/{uuid}/change_quantity")
+    public void changeQuantity(@RequestHeader(name = "username", required = false) String username, @PathVariable String uuid, @RequestParam Long productId, @RequestParam Integer delta) {
+        String targetUuid = getCartUuid(username, uuid);
+        cartService.changeQuantity(targetUuid, productId, delta);
+    }
+
+
+    private String getCartUuid(String username, String uuid) {
+        if (username != null) {
+            return username;
+        }
+        return uuid;
     }
 
 }
