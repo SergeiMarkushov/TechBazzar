bc546df6172c85025a2b4999e94d64074469bf13
aditone <aditone2378@gmail.com>
2023-06-04 19:49:18 +0300

    начата разработка логирования в core

--- Bazzar/core-service/src/main/java/ru/bazzar/core/configs/aop/LoggingAspect.java
+++ Bazzar/core-service/src/main/java/ru/bazzar/core/configs/aop/LoggingAspect.java
@@ -1,9 +1,10 @@
 package ru.bazzar.core.configs.aop;
 
+import lombok.extern.slf4j.Slf4j;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.springframework.stereotype.Component;
-
+@Slf4j
 @Component
 @Aspect
 public class LoggingAspect {
@@ -12,6 +13,5 @@ public class LoggingAspect {
     public void beforeAllMethodInProductServiceImplAdvice(){
         System.out.println("Вызван метод, тут будет логирование");
     }
-
 
 }
