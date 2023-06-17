a3277b5aa929f1b209aa786aa039bc43f1c565a5
aditone <aditone2378@gmail.com>
2023-06-17 18:11:03 +0300

    pic-tests pic-test -тесты сервисного слоя 1

--- Bazzar/picture-service/src/test/java/ru/bazzar/picture/service/PictureServiceTest.java
+++ Bazzar/picture-service/src/test/java/ru/bazzar/picture/service/PictureServiceTest.java
@@ -1,12 +1,13 @@
 package ru.bazzar.picture.service;
 
 import lombok.extern.slf4j.Slf4j;
-import org.checkerframework.checker.units.qual.A;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.function.Executable;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import ru.bazzar.picture.api.ResourceNotFoundException;
 import ru.bazzar.picture.entities.Picture;
 import ru.bazzar.picture.repositories.PictureRepository;
 import ru.bazzar.picture.util.FileResourcesUtils;
@@ -17,6 +18,7 @@ import java.io.InputStream;
 @Slf4j
 class PictureServiceTest {
     private Picture testPicture;
+    private Picture bigPicture;
 
     @Autowired
     private PictureService pictureService;
@@ -26,27 +28,47 @@ class PictureServiceTest {
 
     @BeforeEach
     void setUp() {
-        initPicture();
+        initPictures();
     }
 
     @Test
+    //!
     void saveMultipartFile() {
     }
 
     @Test
+    //сохраняем маленькую картинку
     void save() {
         Assertions.assertNotNull(testPicture);
-        Assertions.assertNotNull(testPicture.getId());
         Assertions.assertEquals(testPicture.getContentType(), "image/jpeg");
         Assertions.assertEquals(testPicture.getFileName(), "test.jpeg");
         Assertions.assertNotNull(testPicture.getBytes());
 
         Picture savedPic = pictureRepository.save(testPicture);
         Assertions.assertEquals(savedPic.getFileName(),testPicture.getFileName());
+        Assertions.assertEquals(savedPic.getBytes(),testPicture.getBytes());
     }
 
     @Test
+    //сохраняем большую картинку
+    //В настройках сервиса стоит ограничение, это проверка возможности сохранения > 5Mb
+    void saveBigPic() {
+        Assertions.assertNotNull(bigPicture);
+        Assertions.assertEquals(bigPicture.getContentType(), "image/jpeg");
+        Assertions.assertEquals(bigPicture.getFileName(), "big.jpg");
+        Assertions.assertNotNull(bigPicture.getBytes());
+
+        Picture savedPic = pictureRepository.save(bigPicture);
+        Assertions.assertEquals(savedPic.getFileName(),bigPicture.getFileName());
+        Assertions.assertEquals(savedPic.getBytes(),bigPicture.getBytes());
+    }
+
+
+    @Test
     void findById() {
+        //ведём поиск по дефолтной фотке
+        // (должна быть всегда! "pic_example/defaultnophotopic.jpg")
+        // добавляется при первой инициализации БД в PictureConfig.class
         Long defaultId = 1L;
 
         Picture foundPic = pictureService.findById(defaultId);
@@ -56,21 +78,35 @@ class PictureServiceTest {
 
     @Test
     void findByFileName() {
+        //не используемый метод
+        Picture foundPic = pictureService.findByFileName("defaultnophotopic.jpg");
+        Assertions.assertNotNull(foundPic);
+        Assertions.assertEquals(foundPic.getFileName(), "defaultnophotopic.jpg");
     }
 
     @Test
     void deleteById() {
+        pictureService.save(testPicture);
+        Long id = pictureService.findByFileName(testPicture.getFileName()).getId();
+        pictureService.deleteById(id);
+
+      Assertions.assertThrows(ResourceNotFoundException.class,
+            ()->{ pictureService.findById(id); });
     }
 
-    private void initPicture() {
+    private void initPictures() {
         FileResourcesUtils fileResourcesUtils = new FileResourcesUtils();
         InputStream is = fileResourcesUtils.getFileFromResourceAsStream("pic_example/test.jpeg");
+        InputStream isBig = fileResourcesUtils.getFileFromResourceAsStream("pic_example/big.jpg");
         testPicture = Picture.builder()
-                //.id(PIC_TEST_ID) -- id переназначается в БД!
                 .contentType("image/jpeg")
                 .fileName("test.jpeg")
                 .bytes(fileResourcesUtils.convertStreamToByteArr(is))
                 .build();
-
+        bigPicture = Picture.builder()
+                .contentType("image/jpeg")
+                .fileName("big.jpg")
+                .bytes(fileResourcesUtils.convertStreamToByteArr(isBig))
+                .build();
     }
 }
