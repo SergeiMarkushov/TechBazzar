c84464ef6d0e0bf913ffb999e754a8598c932ecf
aditone <aditone2378@gmail.com>
2023-06-18 20:55:51 +0300

    сделаны тесты для контроллера картинок

--- Bazzar/picture-service/src/test/java/ru/bazzar/picture/controller/PictureControllerTest.java
+++ Bazzar/picture-service/src/test/java/ru/bazzar/picture/controller/PictureControllerTest.java
@@ -1,76 +1,132 @@
 package ru.bazzar.picture.controller;
 
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.http.MediaType;
+import org.springframework.http.client.MultipartBodyBuilder;
+import org.springframework.test.web.reactive.server.WebTestClient;
+import org.springframework.web.reactive.function.BodyInserters;
+import ru.bazzar.picture.api.PictureDto;
+import ru.bazzar.picture.api.ResourceNotFoundException;
 import ru.bazzar.picture.converters.PictureConverter;
 import ru.bazzar.picture.entities.Picture;
-import lombok.extern.slf4j.Slf4j;
-import ru.bazzar.picture.service.PictureService;
+import ru.bazzar.picture.repositories.PictureRepository;
 import ru.bazzar.picture.util.FileResourcesUtils;
 
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-
-import static org.mockito.Mockito.*;
+import java.io.InputStream;
 
 @Slf4j
-@DisplayName("Контроллер картинок должен")
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
+@AutoConfigureWebTestClient
 class PictureControllerTest {
-    private static final Long PIC_TEST_ID  = 666L;
 
-    private PictureController pictureController;
-    private Picture testPicture;
-    FileResourcesUtils fileResourcesUtils;
+    @Autowired
+    private PictureRepository pictureRepository;
+
+    @Autowired
+    private WebTestClient webTestClient;
+
+    @Autowired
+    private PictureConverter pictureConverter;
+
+    Picture smallPicture;
 
     @BeforeEach
     void setUp() {
         initPicture();
-        // создаем объект-заглушку
-        PictureService mockService = mock(PictureService.class);
-        PictureConverter mockConverter = mock(PictureConverter.class);
-        pictureController = new PictureController(mockService, mockConverter);
-        // задаем нужное поведение объекта-заглушки
-        when(mockService.findById(testPicture.getId())).thenReturn(testPicture);
-
-        fileResourcesUtils = new FileResourcesUtils();
+    }
+    @Test
+    void notFound(){
+        pictureRepository.deleteAll();
+        webTestClient.get()
+              .uri("/api/v1/picture/1")
+              .exchange()
+              .expectStatus().isNotFound();
     }
 
     @Test
-    void findPictureByIdAndReturnDto() {
+    void test_findPictureByIdAndReturnDto() {
+        PictureDto pictureDtoByHttp =
+                webTestClient.get()
+                .uri("/api/v1/picture/find-pic-dto/1")
+                .exchange()
+                .expectStatus().isOk()
+                .expectBody(PictureDto.class)
+                .returnResult()
+                .getResponseBody();
 
+        assert pictureDtoByHttp != null;
+        Assertions.assertEquals(pictureDtoByHttp.getId(),1L);
     }
 
     @Test
-    void savePicDtoAndReturnId() {
+    void test_savePicDtoAndReturnId() {
+        Long pictureIdAfterSaveDtoByHttp =
+                webTestClient.post()
+                        .uri("/api/v1/picture/save-dto-return-id")
+                        .body(BodyInserters.fromValue(pictureConverter.entityToDto(smallPicture)))
+                        .exchange()
+                        .expectStatus().isOk()
+                        .expectBody(Long.class)
+                        .returnResult()
+                        .getResponseBody();
+        assert pictureIdAfterSaveDtoByHttp != null;
+        Picture pictureDB = pictureRepository.
+                findById(pictureIdAfterSaveDtoByHttp).
+                orElseThrow(()-> new ResourceNotFoundException("Картинка не найдена!"));
+        Assertions.assertEquals(pictureDB.getId(),pictureIdAfterSaveDtoByHttp);
     }
 
     @Test
-    void findPictureByIdAndResponse() {
+    void test_findPictureByIdAndResponse() {
+        webTestClient.get()
+                        .uri("/api/v1/picture/1")
+                        .exchange()
+                        .expectStatus().isOk();
     }
 
     @Test
-    void deletePic() {
+    void test_deletePic() {
+        pictureRepository.save(smallPicture);
+        Long id = pictureRepository
+                .findByFileName(smallPicture.getFileName()).get().getId();
+
+        webTestClient.delete()
+               .uri("/api/v1/picture/delete/"+id)
+               .exchange()
+               .expectStatus().isOk();
     }
 
-    @DisplayName("сохранить картинку")
     @Test
-    void savePic() {
+    void test_savePic() {
+        MultipartBodyBuilder multipartBodyBuilder = new MultipartBodyBuilder();
+        multipartBodyBuilder.part(
+                "multipart-pic"
+                        ,new ClassPathResource("pic_example/test.jpeg"))
+                .contentType(MediaType.MULTIPART_FORM_DATA);
 
+        webTestClient.post()
+                .uri("/api/v1/picture/save")
+                .body(BodyInserters.fromMultipartData(multipartBodyBuilder.build()))
+                .exchange()
+                .expectStatus().isCreated();
     }
 
     private void initPicture() {
-        testPicture = new Picture();
-        testPicture.setId(PIC_TEST_ID);
-        testPicture.setFileName("test.jpg");
-        //testPicture.setBytes(new ByteArrayInputStream(new byte[100]).readAllBytes());
-        try {
-            testPicture.setBytes(Files.readAllBytes(Path.of("pic_example/test.jpeg")));
-        } catch (IOException e) {
-            log.error("IOException in testPicture.setBytes() method (testing)");
-            throw new RuntimeException(e);
-        }
+        FileResourcesUtils fileResourcesUtils = new FileResourcesUtils();
+        InputStream is = fileResourcesUtils.getFileFromResourceAsStream("pic_example/test.jpeg");
+        //до 1Mb
+        smallPicture = Picture.builder()
+                .contentType("image/jpeg")
+                .fileName("test.jpeg")
+                .bytes(fileResourcesUtils.convertStreamToByteArr(is))
+                .build();
     }
 
 }
