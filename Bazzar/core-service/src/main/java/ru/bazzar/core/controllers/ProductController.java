a52994a10615298ba0db21b464a179d0b916d8cc
SeregaMarkush <s.markushov@icloud.com>
2023-06-28 20:41:58 +0300

    редактирование

--- Bazzar/core-service/src/main/java/ru/bazzar/core/controllers/ProductController.java
+++ Bazzar/core-service/src/main/java/ru/bazzar/core/controllers/ProductController.java
@@ -133,7 +133,7 @@ public class ProductController {
 
     @GetMapping("/{productId}/average-rating")
     public ResponseEntity<Double> getProductAverageRating(@PathVariable Long productId) {
-        double averageRating = productService.calculateAverageRating(productId);
+        double averageRating = service.calculateAverageRating(productId);
         return ResponseEntity.ok(averageRating);
     }
 }
