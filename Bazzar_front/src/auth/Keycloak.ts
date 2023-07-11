c71235e4d84cee53bc88299a138bc33f6e953190
BekhterNikolay <n.v.bekhter@mail.ru>
2023-07-11 22:14:33 +0300

    изменил хост

--- Bazzar_front/src/auth/Keycloak.ts
+++ Bazzar_front/src/auth/Keycloak.ts
@@ -1,7 +1,7 @@
 import Keycloak from "keycloak-js";
 
 export const keycloak = new Keycloak({
-    url: "http://45.147.177.192:8080/auth/",
+    url: "http://localhost:8080/auth/",
     realm: "Tech Bazzar",
     clientId: "FrontNodeJS",
 });
